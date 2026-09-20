675274d256781c74f158c0b82cf8395739a124be
Snix52 <emrullahs52@icloud.com>
2026-09-20 14:38:51 +0300

    qa: Demir Santrifüj allowlist

--- tools/build_validate.py
+++ tools/build_validate.py
@@ -380,6 +380,8 @@ def allowed(a,p):
         return p in ('/description','/shortdescription','/category')
     if a=='objects/bees/ironcentrifuge/ironcentrifuge.object':
         return p in ('/description','/shortdescription','/subtitle','/category')
+    if a=='objects/bees/ironcentrifuge/ironcentrifuge.object':
+        return p in ('/description','/shortdescription','/subtitle','/category')
     if a=='objects/crafting/armory/armory.object':
         return p in ('/description','/shortdescription') or bool(re.fullmatch(r'/upgradeStages/[012]/(itemSpawnParameters/(description|shortdescription)|interactData/paneLayoutOverride/lbl(Title|SubTitle)/value)',p))
     if a=='objects/crafting/upgradeablecraftingobjects/craftinganvil/craftinganvil.object':
